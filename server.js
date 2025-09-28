8af55174e2f5c2fb40dc7f03189a97407e8c6ea3
Whoistolu <digittolu25@gmail.com>
2025-09-28 23:14:21 +0100

    `Add new route "/dashboard" and updated server.js file`

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ app.get("/", (req, res) => {
     console.log("My server is running", req.method)
     res.sendStatus(200)
 })
+
+app.get("/dashboard", (req, res) => {
+    console.log("Dashboard route accessed")
+    res.send("Welcome to the Dashboard")
+})
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`)) 
